29a8c10d37027fd7c1760d76110e71d157d8a020
cyro <joshuawang2048@gmail.com>
2025-03-12 20:31:19 -0700

    added input component

--- client/app/openScreen/index.jsx
+++ client/app/openScreen/index.jsx
@@ -1,19 +1,33 @@
 import { ThemedView } from "../../components/ThemedView";
 import { ThemedText } from "../../components/ThemedText";
+import { ThemedInput } from "../../components/ThemedInput";
 import { StyleSheet } from "react-native";
 
 export default function openScreen() {
   return (
     <ThemedView style={styles.container}>
       <ThemedText type="title">Not Logged In?</ThemedText>
+      <ThemedView style={styles.inputContainer}>
+        <ThemedInput style={styles.input} placeholder="Email" />
+        <ThemedInput style={styles.input} placeholder="Password" />
+      </ThemedView>
     </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 100,
     flex: 1,
     alignItems: "center",
+    justifyContent: "center",
+    paddingBottom: 20,
+    padding: 30,
+  },
+  inputContainer: {
+    width: "100%",
+    marginTop: 20,
+  },
+  input: {
+    width: "100%",
   },
 });
---
+++ client/components/ThemedInput.jsx
@@ -0,0 +1,61 @@
+import { TextInput, StyleSheet, Text } from "react-native";
+import { useThemeColor } from "../hooks/useThemeColor";
+
+export function ThemedInput({
+  style,
+  placeholder,
+  lightColor,
+  darkColor,
+  type = "default",
+  editable = true,
+  required = false,
+  ...rest
+}) {
+  const color = useThemeColor({ light: lightColor, dark: darkColor }, "text");
+  const isDisabled = type === "disabled" || !editable;
+  const isReadOnly = type === "read-only";
+
+  const modifiedPlaceholder = required ? `${placeholder} *` : placeholder;
+
+  return (
+    <TextInput
+      style={[
+        styles.input,
+        { color },
+        isDisabled && styles.disabled,
+        isReadOnly && styles.readOnly,
+        style,
+      ]}
+      placeholder={modifiedPlaceholder}
+      editable={!isDisabled && !isReadOnly}
+      selectTextOnFocus={!isDisabled}
+      pointerEvents={isDisabled ? "none" : "auto"}
+      {...rest}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    padding: 14,
+    paddingLeft: 10,
+    paddingRight: 10,
+    borderWidth: 1,
+    borderColor: "rgba(255, 255, 255, 0.1)",
+    backgroundColor: "rgba(255, 255, 255, 0.1)",
+    borderRadius: 12,
+    marginBottom: 10,
+    minWidth: 200,
+    fontSize: 16,
+    fontFamily: "Montserrat",
+  },
+  disabled: {
+    opacity: 0.5,
+    backgroundColor: "rgba(200, 200, 200, 0.1)",
+  },
+  readOnly: {
+    backgroundColor: "transparent",
+    borderColor: "rgba(255, 255, 255, 0.1)",
+    borderWidth: 2,
+  },
+});
