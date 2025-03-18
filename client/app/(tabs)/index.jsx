929fe428cfce00d34d164037a8be08efeae51693
cyro <joshuawang2048@gmail.com>
2025-03-17 20:24:56 -0700

    added basic camera

--- client/app/(tabs)/index.jsx
+++ client/app/(tabs)/index.jsx
@@ -1,26 +1,48 @@
-import { Image, StyleSheet, Platform } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import { CameraView, useCameraPermissions } from "expo-camera";
 
-export default function HomeScreen() {
-  return <></>;
+export default function Camera() {
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  return (
+    <ThemedView style={styles.container}>
+      <CameraView style={styles.camera} facing="back">
+        <ThemedText style={styles.title}>Focus Camera On Item</ThemedText>
+      </CameraView>
+    </ThemedView>
+  );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
+  container: {
+    flex: 1,
+    justifyContent: "center",
     alignItems: "center",
-    gap: 8,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  title: {
+    fontSize: 24,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
+  camera: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
