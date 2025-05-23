ebe9538e1b953b83c0680ccd23e3de333b49eafe
cyro <joshuawang2048@gmail.com>
2025-05-22 18:02:24 -0700

    main commit

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
 require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const os = require("os");
 
 const { initializeDatabase } = require("./db");
 const api = require("./api/api");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,23 +11,10 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use("/api", api);
 
-function getLocalIp() {
-  const interfaces = os.networkInterfaces();
-  for (const name of Object.keys(interfaces)) {
-    for (const iface of interfaces[name]) {
-      if (iface.family === "IPv4" && !iface.internal) {
-        return iface.address;
-      }
-    }
-  }
-  return "localhost";
-}
-
 async function startServer() {
   await initializeDatabase();
   app.listen(PORT, "0.0.0.0", () => {
-    const ip = getLocalIp();
-    console.log(`Server running on http://${ip}:${PORT}`);
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
   });
 }
 
